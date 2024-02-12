8200107b036b1a25346f23e4a6b327ab3f76496b
Jonas <140553581+jnsdotgg@users.noreply.github.com>
2024-02-12 09:56:00 +0100

    made it not discard the undo stack on format

--- assets/format.js
+++ assets/format.js
@@ -1,17 +1,21 @@
-import * as prettier from "https://unpkg.com/prettier@3.2.5/standalone.mjs";
-import prettierPluginBabel from "https://unpkg.com/prettier@3.2.5/plugins/babel.mjs";
-import prettierPluginEstree from "https://unpkg.com/prettier@3.2.5/plugins/estree.mjs";
-import prettierPluginHtml from "https://unpkg.com/prettier@3.2.5/plugins/html.mjs";
+import * as prettier from "https://unpkg.com/prettier@3.2.5/standalone.mjs"
+import prettierPluginBabel from "https://unpkg.com/prettier@3.2.5/plugins/babel.mjs"
+import prettierPluginEstree from "https://unpkg.com/prettier@3.2.5/plugins/estree.mjs"
+import prettierPluginHtml from "https://unpkg.com/prettier@3.2.5/plugins/html.mjs"
 
 const formatBtn = document.getElementById("format-btn")
 
 formatBtn.addEventListener("click", async () => {
-    window.editor.setValue(
-        await prettier.format(window.editor.getValue(), {
+
+
+    window.editor.executeEdits("", [{
+        range: window.editor.getModel().getFullModelRange(),
+        text: await prettier.format(window.editor.getValue(), {
             parser: "html",
             plugins: [prettierPluginBabel, prettierPluginEstree, prettierPluginHtml],
         }),
-    );
+        // forceMoveMarkers: true
+    }])
 
     nToast("Successfully formatted code")
 })
