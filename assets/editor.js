55500ea996f3bbc55ae21798c9b6558e00f79543
Jonas <140553581+jnsdotgg@users.noreply.github.com>
2024-02-28 17:45:50 +0100

    fix

--- assets/editor.js
+++ assets/editor.js
@@ -366,7 +366,7 @@ function share(mode) {
         const newURL = base64Compressed ? `${currentURL}?c=${base64Compressed}` : currentURL
 
         if (mode === "short") {
-            const embedLink = `https://embed.jns.gg/${encodeURIComponent(extractTitle())}?provider=JnsCode+-+Anonymous+code+sharing&providerurl=https://code.jns.gg/&color=f59e0b&image=https://code.jns.gg/assets/logo-32x32.png&imagetype=thumbnail&redirect=${newURL}`
+            const embedLink = `https://embed.jns.gg/${encodeURIComponent(extractTitle()).replace("%20", "+")}?provider=JnsCode+-+Anonymous+code+sharing&providerurl=https://code.jns.gg/&color=f59e0b&image=https://code.jns.gg/assets/logo-32x32.png&imagetype=thumbnail&redirect=${newURL}`
             var formData = new FormData()
             formData.append("url", embedLink)
 
